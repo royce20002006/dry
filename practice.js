43174146cc2c1af22ae31e63cf8b0042d98c068e
Royce Calvin <royce20002006@yahoo.com>
2024-01-09 22:42:48 -0600

    finished with dry

--- practice.js
+++ practice.js
@@ -1,44 +1,40 @@
 function multiplyBiggerNumByTwo(num1, num2) {
-  let bigNum;
-  if (num1 > num2) {
-    bigNum = num1;
-    return bigNum * 2;
-  } else {
-    bigNum = num2;
-    return bigNum * 2;
-  }
+ 
+  let bigNum = check(num1, num2)
+  return bigNum * 2;
 }
 
+
 function divideBiggerNumByThree(num1, num2) {
-  let bigNum;
-  if (num1 > num2) {
-    bigNum = num1;
-    return bigNum / 3;
-  } else {
-    bigNum = num2;
-    return bigNum / 3;
-  }
+  let bigNum = check(num1, num2)
+  return bigNum / 3;
+  
 }
 
 function eatMostTacos(sum1, sum2) {
-  let bigNum;
-  if (sum1 > sum2) {
-    bigNum = sum1;
+  let bigNum = check(sum1, sum2)
     return `I ate ${bigNum} tacos.`;
-  } else {
-    bigNum = sum2;
-    return `I ate ${bigNum} tacos.`;
-  }
+  
 }
 
 function adoptSmallerDog(weight1, weight2) {
   let smallDog;
   if (weight1 < weight2) {
     smallDog = weight1;
-    return `I adopted a dog that weighs ${smallDog} pounds.`;
-  } else {
+   } else {
     smallDog = weight2;
-    return `I adopted a dog that weighs ${smallDog} pounds.`;
+   }
+   return `I adopted a dog that weighs ${smallDog} pounds.`;
+}
+
+let check = (one, two) => {
+  
+  if (one > two) {
+    bigNum = one;
+    return bigNum;
+  } else {
+    bigNum = two;
+    return bigNum
   }
 }
 
